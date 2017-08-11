37aaf0bc804d1d162bc8db8b84725c534d81411e
Juan Lopez <jc.lopez11@uniandes.edu.co>
2017-08-11 12:11:54 -0500

    clase1

---
+++ clase1/taller1.R
@@ -0,0 +1,25 @@
+#Taller1 Bioestadistica
+#Mauricio Neira 201424001
+#Miguel Rodriguez 201411063
+
+prueba <- c( 12, 13, 19.3, 18.9, 19.6, 13.5, 33.2, 54.1, 18.5 )
+
+cat("PUNTO1:\n\n")
+
+cat("prueba:\n",prueba,"\n\n")
+
+cat("prueba^2:\n",prueba**2,"\n\n")
+
+cat("(2*prueba)+(3*prueba^2)-1:\n",(2*prueba)+(3*(prueba**2))-1,"\n\n")
+
+cat("1/prueba^2:\n",1/prueba**2,"\n\n")
+
+cat("log(prueba^2/(3*prueba^4)):\n",log(prueba^2/(3*prueba^4)),"\n\n")
+
+
+cat("PUNTO2:\n\n")
+
+help.search("correlation")
+break
+
+cat("hola")
